bb30720692b656863ba759c67fe06462973e0700
Adobe-Android <tngamerboy@gmail.com>
2017-06-21 13:47:00 -0500

    basic module structure and event listeners complete

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ let checkboxes = document.getElementById("chatBody");
 
 document.getElementById("dark-theme").addEventListener("click", function(){
 	checkboxes.classList.toggle("dark-theme");
-});
+}());
 
 document.getElementById("large-text").addEventListener("click", function(){
 	checkboxes.classList.toggle("large-text");
-});
+}());
---
+++ messagesDelete.js
@@ -0,0 +1,11 @@
+
+var userInput = document.getElementById("message-input")
+userMessage.target.addEventListener("keypress", function(){
+// invokes function to take any keypress to submit the user input to the DOM.
+}());
+
+var userOutput = document.getElementById("message-output")
+userOutput.target.addEventListener("click", function(){
+// takes a click event and clears the DOM
+// removes the corresponding message from the private array
+}());
