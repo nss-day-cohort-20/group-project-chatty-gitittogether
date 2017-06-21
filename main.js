62bbdc3214316ec41ac6b85b0cac81f56634123f
Adobe-Android <tngamerboy@gmail.com>
2017-06-21 14:58:57 -0500

    We made CSS changes to more closely match the moqups page

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-let myRequest = new XMLHttpRequest();
+var chatChatChat = (function(oldChatChat) {
+    let myRequest = new XMLHttpRequest();
 
-let checkboxes = document.getElementById("chatBody");
+    let checkboxes = document.getElementById("chatBody");
 
-document.getElementById("dark-theme").addEventListener("click", function(){
-	checkboxes.classList.toggle("dark-theme");
-}());
+    document.getElementById("dark-theme").addEventListener("click", function() {
+        checkboxes.classList.toggle("dark-theme");
+    });
 
-document.getElementById("large-text").addEventListener("click", function(){
-	checkboxes.classList.toggle("large-text");
-}());
+    document.getElementById("large-text").addEventListener("click", function() {
+        checkboxes.classList.toggle("large-text");
+    });
+}(chatChatChat || {}));
