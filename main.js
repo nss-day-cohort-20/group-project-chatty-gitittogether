846af1fe57b951e76f7bb9c85f15808a2acc68ee
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-06-21 11:37:43 -0500

    code was not working.. inspected html and css were calling id dark theme - dark themed fixed and now works

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ let myRequest = new XMLHttpRequest();
 
 let checkboxes = document.getElementById("chatBody");
 
-document.getElementById("dark-themed").addEventListener("click", function(){
-	checkboxes.classList.toggle("dark-themed");
+document.getElementById("dark-theme").addEventListener("click", function(){
+	checkboxes.classList.toggle("dark-theme");
 });
 
 document.getElementById("large-text").addEventListener("click", function(){
