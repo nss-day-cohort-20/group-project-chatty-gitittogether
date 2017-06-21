9b9b539e0be66ea9d51c12c6f7d4115902cfe34b
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-06-21 11:30:33 -0500

    added event listeners to checkboxes id linking them to target body id = chatBody then added css themes

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 let myRequest = new XMLHttpRequest();
+
+let checkboxes = document.getElementById("chatBody");
+
+document.getElementById("dark-themed").addEventListener("click", function(){
+	checkboxes.classList.toggle("dark-themed");
+});
+
+document.getElementById("large-text").addEventListener("click", function(){
+	checkboxes.classList.toggle("large-text");
+});
