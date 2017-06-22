80d5b73c2f7031e922b0cd51e5120c2bd383890d
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-06-22 11:05:21 -0500

    trying to firgure out how to only enable and disable the clear message button... if else statement?

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 let checkboxes = document.getElementById("chatBody");
+var darkTheme = document.getElementById("dark-theme");
+var largeText =  document.getElementById("large-text");
+var clearButton = document.getElementById("clear-btn");
 
-document.getElementById("dark-theme").addEventListener("click", function() {
+darkTheme.addEventListener("click", function() {
   checkboxes.classList.toggle("dark-theme");
 });
 
-document.getElementById("large-text").addEventListener("click", function() {
+largeText.addEventListener("click", function() {
   checkboxes.classList.toggle("large-text");
+});
+
+
+// TODO First, create logic to toggle the button to be disabled on click
+clearButton.addEventListener("click", function(){
+	checkboxes.classList.toggle("clear-btn");
+// TODO Second, hook up the logic to the arrays.
+if (//when clicked there is is content then run){
+
+} else {disabled}
 });
 
 let loadMessages = function(msg){
