4b1b0de366eeb0c15b0aff320c56bdf2f7613376
Adobe-Android <tngamerboy@gmail.com>
2017-06-23 14:27:42 -0500

    Made a function to clear the text box

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
-let checkboxes = document.getElementById("chatBody");
+// let checkboxes = document.getElementById("chatBody");
 var darkTheme = document.getElementById("dark-theme");
-var largeText =  document.getElementById("large-text");
+var largeText = document.getElementById("large-text");
 var clearBtn = document.getElementById("clear-btn");
-
+let outputMessageArea = document.getElementById("message-output")
 
 
 darkTheme.addEventListener("click", function() {
-  checkboxes.classList.toggle("dark-theme");
+    outputMessageArea.classList.toggle("dark-theme");
 });
 
 largeText.addEventListener("click", function() {
-  checkboxes.classList.toggle("large-text");
+    outputMessageArea.classList.toggle("large-text");
 });
 
 
-clearBtn.addEventListener("click", function(){
-	clearBtn.classList.toggle("clear-btn");
-})
+// clearBtn.addEventListener("click", function(){
+//  if (outputMessageArea.contains(messageHolder)) {
+//    clearBtn.setAttribute("disabled", "disabled");
+//    }
+//  clearBtn.classList.toggle("clear-btn");
+// })
 
-clearBtn.setAttribute("disabled", "disabled");
+
 // clearBtn.removeAttribute("disabled");
 // enableBtn.addEventListener("click", function(){
 
@@ -27,33 +30,58 @@ clearBtn.setAttribute("disabled", "disabled");
 
 // ****************************JS & MB********************************
 let messagesHub = function(messagesArr) {
-	chatChatChat.addInitialMessages(messagesArr);
-	messagesAddToDOM();
+    chatChatChat.addInitialMessages(messagesArr);
+    messagesAddToDOM();
 }
 
 let messagesAddToDOM = function() {
-	let outputMessageArea = document.getElementById("message-output");
-	allmessages = chatChatChat.getMessages();
+    allmessages = chatChatChat.getMessages();
+
+    allmessages = allmessages.map(function(currentMessage) {
+        let messageHolder = document.createElement("div");
+        messageHolder.setAttribute("class", "eachMessage");
+        messageHolder.innerHTML = currentMessage.msg;
+        outputMessageArea.appendChild(messageHolder)
+
+        let deleteBtn = document.createElement("button");
+        deleteBtn.setAttribute("class", "removeMessage");
+        deleteBtn.innerHTML = "Delete";
+
+        messageHolder.appendChild(deleteBtn)
+
+        deleteBtn.addEventListener("click", function() {
+            outputMessageArea.removeChild(messageHolder)
+        })
+        clearBtn.addEventListener("click", function() {
+            if (outputMessageArea.contains(messageHolder)) {
+                outputMessageArea.removeChild(messageHolder)
+            } else {
+                clearBtn.setAttribute("disabled", "disabled");
+            }
+            console.log("buttonworks");
+        })
+    })
+}
+
+let userInput = document.getElementById("message-input");
+
+function clearTextBox(event, inputText) {
+  if (inputText != "") {
+    userInput.value = "";
+  }
+};
+
+userInput.addEventListener("keypress", function(event) {
+    // invokes function to take any keypress to submit the user input to the DOM.
+    if (event.key === "Enter") {
+        let inputText = userInput.value;
+        chatChatChat.addNewMsgToArr(inputText);
+        clearTextBox(inputText);
+    }
+});
 
 
-	allmessages = allmessages.map(function(currentMessage){
-		let messageHolder = document.createElement("div");
-		messageHolder.setAttribute("class", "eachMessage");
-		messageHolder.innerHTML = currentMessage.msg;
-		outputMessageArea.appendChild(messageHolder)
-	
-		let deleteBtn = document.createElement("button");
-		deleteBtn.setAttribute("class", "removeMessage");
-		deleteBtn.innerHTML = "Delete";
 
-		messageHolder.appendChild(deleteBtn)
 
-		deleteBtn.addEventListener("click", function(){
-			outputMessageArea.removeChild(messageHolder)
-		})
-	})
-}	
-
+console.log(chatChatChat)
 chatChatChat.loadInitialMessages(messagesHub);
-
-
